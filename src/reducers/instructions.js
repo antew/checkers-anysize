878dd9beb01d38384ab47ad7c5504f697565fc18
Antew <antewcode@gmail.com>
2017-07-02 16:14:20 -0400

    Reset instructions when restarting the game.

--- src/reducers/instructions.js
+++ src/reducers/instructions.js
@@ -5,6 +5,10 @@ export default (instructions = initialState, action) => {
   switch (action.type) {
     case GameActions.SET_INSTRUCTIONS:
       return action.instructions;
+
+    case GameActions.RESET_GAME:
+      return initialState;
+
     default:
       return instructions;
   }
